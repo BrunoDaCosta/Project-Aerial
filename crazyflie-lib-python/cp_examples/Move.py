cf512f0c87142681ed60a892b7dc8f202e2ee487
AlexClivaz <al.clivaz@gmail.com>
2021-05-07 19:32:27 +0200

    Avoid obstacle near border + position

--- crazyflie-lib-python/cp_examples/Move.py
+++ crazyflie-lib-python/cp_examples/Move.py
@@ -23,16 +23,14 @@
 #  MA  02110-1301, USA.
 """
 This script shows the basic use of the PositionHlCommander class.
-
 Simple example that connects to the crazyflie at `URI` and runs a
 sequence. This script requires some kind of location system.
-
 The PositionHlCommander uses position setpoints.
-
 Change the URI variable to your Crazyflie configuration.
 """
 import cflib.crtp
 import time
+import sys
 from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.positioning.position_hl_commander import PositionHlCommander
@@ -41,6 +39,17 @@ from cflib.positioning.motion_commander import MotionCommander
 
 # URI to the Crazyflie to connect to
 uri = 'radio://0/10/2M/E7E7E7E7E7'
+x0 = 0
+y0 = 0
+ADVANCE = 1
+GOAL = 2
+CORNER = 3
+
+if len(sys.argv) >1:
+    x0 = float(sys.argv[1])
+    if len(sys.argv) >2:
+        y0 = float(sys.argv[2])
+
 
 def is_close(range):
     MIN_DISTANCE = 0.2  # m
@@ -52,29 +61,75 @@ def is_close(range):
 
 if __name__ == '__main__':
     cflib.crtp.init_drivers(enable_debug_driver=False)
-    with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
-        with MotionCommander(scf) as motion_commander:
-            with PositionHlCommander(
-                    scf,
-                    x=0.0, y=0.0, z=0.0,
-                    default_velocity=0.2,
-                    default_height=0.3,
-                    controller=PositionHlCommander.CONTROLLER_MELLINGER) as pc:
-                with Multiranger(scf) as multiranger:
-                    #init_x, init_y, init_z = pc.get_position()
-                    VELOCITY = 0
-                    while pc.get_position()[0] < 1:
-                        # Obstacle face
+    cf = Crazyflie(rw_cache='./cache')
+    with SyncCrazyflie(uri, cf=cf) as scf:
+        with MotionCommander(
+                scf,
+                default_height = 0.2) as motion_commander:
+            with Multiranger(scf) as multiranger:
+                VELOCITY = 0.2
+                keep_flying = True
+                x = x0
+                y = y0
+                STATE = ADVANCE
+                time.sleep(1)
+                while keep_flying:
+                    vx = 0
+                    vy = 0
+                    # Main loop of the controller
+                    if x > 3.5:
+                        STATE = GOAL
+                        
+                    if STATE == ADVANCE:
                         if (is_close(multiranger.front)):
                             if (is_close(multiranger.right)):
                                 print("Left")
-                                motion_commander.start_linear_motion(0, VELOCITY, 0)
-                            else:
+                                if y > 2.8:
+                                    STATE = CORNER
+                                else:
+                                    vy = VELOCITY
+                            elif (is_close(multiranger.left)):
                                 print("Right")
-                                motion_commander.start_linear_motion(0, -VELOCITY, 0)
+                                if y < 0.2:
+                                    STATE = CORNER
+                                else :
+                                    vy = -VELOCITY
+                            else:
+                                if y < 1.5:
+                                    vy = VELOCITY
+                                else:
+                                    vy = -VELOCITY
                         else:
                             print("Front")
-                            motion_commander.start_linear_motion(VELOCITY, 0, 0)
-                        time.sleep(0.1)
-                        print("Pos x:" + str(pc.get_position()[0]) + " Pos y:" + str(pc.get_position()[1]))
-                    pc.land()
+                            vx = VELOCITY
+                    elif STATE == CORNER:
+                        if y < 1.5:
+                            if (is_close(multiranger.left)):
+                                vx = -VELOCITY
+                            else:
+                                motion_commander.start_linear_motion(0, VELOCITY, 0)
+                                time.sleep(0.5)
+                                y += VELOCITY * 0.5
+                                if not(is_close(multiranger.front)):
+                                    STATE = ADVANCE
+                        else:
+                            if (is_close(multiranger.right)):
+                                vx = -VELOCITY
+                            else:
+                                motion_commander.start_linear_motion(0, -VELOCITY, 0)
+                                time.sleep(0.5)
+                                y -= VELOCITY * 0.5
+                                if not(is_close(multiranger.front)):
+                                    STATE = ADVANCE
+                    elif STATE == GOAL:
+                        pass
+                    
+                    motion_commander.start_linear_motion(vx, vy, 0)
+                    x += vx * 0.1
+                    y += vy * 0.1
+                    if is_close(multiranger.up):
+                        print("landing")
+                        keep_flying = False
+                    time.sleep(0.1)
+                    print("{}  {}".format(x,y))
+
